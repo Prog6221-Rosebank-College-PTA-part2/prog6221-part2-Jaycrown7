f6814a1fb9239182957782dd9af99f0e09c4315d
Jaycrown7 <st10472632@rcconnect.edu.za>
2026-05-29 21:02:31 +0200

    Initial commit

---
+++ BOTGUI/BOTGUI/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BOTGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
